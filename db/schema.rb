54eeafd7e5c91e12b9e73bf7bc0828a1769fd2a8
Alan deLevie <alan.delevie@gsa.gov>
2015-01-04 16:35:35 -0500

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150103035213) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "filings", force: :cascade do |t|
     t.string   "docket_number",  null: false
     t.string   "name_of_filer",  null: false
