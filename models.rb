98bc87c1c414ac884650a1a8180baff000fc2052
Alan deLevie <alan.delevie@gsa.gov>
2015-01-10 16:14:38 -0500

    Enabled different databases based on environment

--- models.rb
+++ models.rb
@@ -6,9 +6,11 @@ require 'logger'
 require 'yaml'
 require 'pry'
 
+ezfs_env = ENV.fetch('EZFS_ENV', 'development')
+
 ActiveRecord::Base.logger = Logger.new('debug.log')
 configuration = YAML::load(IO.read('db/config.yml'))
-ActiveRecord::Base.establish_connection(configuration['development'])
+ActiveRecord::Base.establish_connection(configuration[ezfs_env])
 
 class Filing < ActiveRecord::Base
   validates_uniqueness_of :fcc_id
