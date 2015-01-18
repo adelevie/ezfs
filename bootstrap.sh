9fb16f7acfec40ea5a1bc28822a64becaa9b0cc2
Alan deLevie <alan.delevie@gsa.gov>
2015-01-17 20:10:24 -0500

    added whenever gem install to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,13 @@
 git pull origin master
 bundle
+
 if [ ! -f db/config.yml ]; then
   cp db/config.yml.sample db/config.yml
   echo "Created config.yml"
   echo "To edit, type:"
   echo "nano db/config.yml"
+fi
+
+if ! gem spec whenever > /dev/null 2>&1; then
+  gem install whenever
 fi
