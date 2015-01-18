80e9b50e98219dcdd2b83ba84857da1ee99300d5
Alan deLevie <alan.delevie@gsa.gov>
2015-01-17 19:32:16 -0500

    no more development environment. RIP 12 factor app...

--- models.rb
+++ models.rb
@@ -5,11 +5,9 @@ require 'logger'
 require 'yaml'
 require 'pry'
 
-ezfs_env = ENV.fetch('EZFS_ENV', 'development')
-
 ActiveRecord::Base.logger = Logger.new('debug.log')
 configuration = YAML::load(IO.read('db/config.yml'))
-ActiveRecord::Base.establish_connection(configuration[ezfs_env])
+ActiveRecord::Base.establish_connection(configuration['production'])
 
 class Filing < ActiveRecord::Base
   validates_uniqueness_of :fcc_id
