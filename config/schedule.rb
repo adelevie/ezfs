77d00805c82bbe63108a0a0c22dc4bbd3a9906bd
Alan deLevie <alan.delevie@gsa.gov>
2015-01-18 23:34:34 -0500

    using the magical Clock class to spread out scrapes

--- config/schedule.rb
+++ config/schedule.rb
@@ -92,8 +92,75 @@ class Docket
   end
 end
 
-every 1.day, at: '6:00 am' do
-  Docket.all.each do |docket_number|
-    rbenv_rake "scrape:track docket_number=#{docket_number}"
+class Clock
+  def initialize(hour, minute, ampm)
+    @hour   = hour
+    @minute = minute
+    @ampm   = ampm
+  end
+  
+  def minute_to_s
+    if @minute < 10
+      "0#{@minute}"
+    else
+      @minute
+    end
+  end
+  
+  def time_to_s
+    "#{@hour}:#{minute_to_s} #{@ampm}"
+  end
+  
+  def inspect
+    "<Clock: #{time_to_s} >"
+  end
+  
+  def toggle_ampm!
+    if @ampm == 'pm'
+      @ampm = 'am'
+    else
+      @ampm = 'pm'
+    end
+  end
+  
+  def add_1_hour!
+    if @hour == 12
+      toggle_ampm!
+    end
+    @hour = @hour + 1
+  end
+  
+  def add_1_minute!
+    if @minute == 59
+      add_1_hour!
+      @minute = 0
+    else
+      @minute = @minute + 1
+    end
+  end
+  
+  def add_n_minutes!(n)
+    n.times do
+      add_1_minute!
+    end
   end
 end
+
+
+@clock = Clock.new(6, 0, 'am')
+
+Docket.all.each_slice(5) do |docket_numbers|
+  every 1.day, at: @clock.time_to_s do
+    docket_numbers.each do |docket_number|
+      rbenv_rake "scrape:track docket_number=#{docket_number}"
+    end
+  end
+  
+  @clock.add_n_minutes!(5)
+end 
+
+# every 1.day, at: '6:00 am' do
+#   Docket.all.each do |docket_number|
+#     rbenv_rake "scrape:track docket_number=#{docket_number}"
+#   end
+# end
