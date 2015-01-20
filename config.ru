255634f7fe26d3cf73bdcc4b3949a3c35fc75bd8
Alan deLevie <alan.delevie@gsa.gov>
2015-01-19 21:05:43 -0500

    modularized sinatra app

---
+++ config.ru
@@ -0,0 +1,3 @@
+# config.ru
+require './webapp'
+run WebApp
--- webapp.rb
+++ webapp.rb
@@ -3,22 +3,26 @@ require 'bundler/setup'
 require 'sinatra'
 require 'pry'
 
-get '/:docket_number' do
-  erb :index, locals: {docket_number: params[:docket_number], results: nil}
-end
+class WebApp < Sinatra::Base
 
-get '/:docket_number/search' do
-  query = params['q']
-  docket_number = params['docket_number']
-  
-  results = Filing.search(where: {docket_number: docket_number, citation: query})
-  if results.length == 0
-    results = Filing.search(query, where: {docket_number: docket_number})
+  get '/:docket_number' do
+    erb :index, locals: {docket_number: params[:docket_number], results: nil}
   end
+
+  get '/:docket_number/search' do
+    query = params['q']
+    docket_number = params['docket_number']
   
-  if results.length == 1
-    redirect results.first.fcc_url
-  else  
-    erb :index, locals: {docket_number: params[:docket_number], results: results}
+    results = Filing.search(where: {docket_number: docket_number, citation: query})
+    if results.length == 0
+      results = Filing.search(query, where: {docket_number: docket_number})
+    end
+  
+    if results.length == 1
+      redirect results.first.fcc_url
+    else  
+      erb :index, locals: {docket_number: params[:docket_number], results: results}
+    end
   end
+
 end
